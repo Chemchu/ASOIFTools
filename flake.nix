38b9b19a636ef5ba8154875e19b0bf0016f0188d
Chemchu <gustavo_9_rj@hotmail.com>
2024-09-15 01:42:10 +0200

    fix: adding ANDROID_HOME

--- flake.nix
+++ flake.nix
@@ -3,17 +3,15 @@
     nixpkgs.url = "github:NixOS/nixpkgs";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
-    android-nixpkgs = {
-      url = "github:tadfisher/android-nixpkgs/stable";
-    };
   };
 
-  outputs = { nixpkgs, rust-overlay, flake-utils, android-nixpkgs, ... }:
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
-        androidSdk = android-nixpkgs.packages.${system};  # Access Android SDK packages
+        pkgs = import nixpkgs { inherit system overlays; config.allowUnfree = true; config.android_sdk.accept_license = true; };
+        androidSdk = (pkgs.android-studio.withSdk (pkgs.androidenv.composeAndroidPackages { includeNDK = true; }).androidsdk);
+        androidComposition = (pkgs.androidenv.composeAndroidPackages { includeNDK = true; });
       in {
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = [ pkgs.pkg-config ];
@@ -24,6 +22,7 @@
               targets = [ "wasm32-unknown-unknown" ];
               profile = "minimal";
             })
+            androidComposition.androidsdk
             pkgs.pkg-config
             pkgs.atk
             pkgs.pango
@@ -33,17 +32,12 @@
             pkgs.cargo-tauri
             pkgs.trunk
             pkgs.systemdLibs
-            androidSdk.cmdline-tools-latest
-            androidSdk.build-tools-34-0-0
-            androidSdk.platform-tools
-            androidSdk.platforms-android-34
-            androidSdk.emulator
           ];
 
           # Use platform-tools as a base for ANDROID_HOME
           shellHook = ''
-            export ANDROID_HOME=${androidSdk.platform-tools}/..  # Navigate up from platform-tools to set ANDROID_HOME
-            export PATH=$ANDROID_HOME/platform-tools:$ANDROID_HOME/emulator:$PATH
+            export ANDROID_HOME=${androidComposition.androidsdk}/libexec/android-sdk
+            export NDK_HOME=$ANDROID_HOME/ndk-bundle
           '';
         };
       }
